c061d2d8186d447fb14f19bce8c6b9b3b5729d1d
Kiminaze <kiminaze@yahoo.de>
2022-12-09 00:59:16 +0100

    Statebag update

---
+++ server.lua
@@ -0,0 +1,15 @@
+
+-- fix vehicle deformation
+function FixVehicleDeformation(vehicle)
+	assert(vehicle and DoesEntityExist(vehicle), "Parameter \"vehicle\" must be a valid vehicle entity!")
+
+	Entity(vehicle).state:set("deformation", {}, true)
+end
+
+RegisterNetEvent("VD:fixDeformation", function(networkId)
+	FixVehicleDeformation(NetworkGetEntityFromNetworkId(networkId))
+end)
+
+
+
+exports("FixVehicleDeformation", FixVehicleDeformation)
